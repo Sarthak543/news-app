a4c8fb9fb9fa7919398e2c1f761dd7f2387460e9
Sarthak543 <sarthakjaiswal543@gmail.com>
2024-01-17 11:16:33 +0530

    fixing height of img and card body

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,8 +11,8 @@ export default class NewsItem extends Component {
                         {source}
                         <span class="visually-hidden">unread messages</span>
                     </span>
-                    <img src={imageURL ? imageURL : `https://source.unsplash.com/300x175/?${category}`} className="card-img-top" alt="..." />
-                    <div className="card-body">
+                    <img src={imageURL ? imageURL : `https://source.unsplash.com/300x175/?${category}`} className="card-img-top" alt="..." style={{height:"12rem"}}/>
+                    <div className="card-body" style={{height:"17rem"}}>
                         <h5 className="card-title">{title}{title.length === 45 ? "..." : ""}</h5>
                         <p className="card-text">{description}...</p>
                         <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
