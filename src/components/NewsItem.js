566ccd49c77efdafd7e7985e51550d82a1dee346
Sarthak543 <sarthakjaiswal543@gmail.com>
2024-01-17 14:03:47 +0530

    added the author name on the news item

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,9 +7,9 @@ export default class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: "20rem" }}>
-                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%"}}>
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:"90%"}}>
                         {source}
-                        <span class="visually-hidden">unread messages</span>
+                        <span className="visually-hidden">unread messages</span>
                     </span>
                     <img src={imageURL ? imageURL : `https://source.unsplash.com/300x175/?${category}`} className="card-img-top" alt="..." style={{height:"12rem"}}/>
                     <div className="card-body" style={{height:"17rem"}}>
